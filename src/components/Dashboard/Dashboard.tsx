a344d8f52b406246ad6e24687c33dc374d893fff
maxsadbek <masadanb@gmail.com>
2026-03-15 11:38:20 +0500

    add

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,13 +1,19 @@
 import ChartSection from "./ChartSection"
 import StatusGrid from "./StatusGrid"
+import TableSection from "./TableSection"
 
 const Dashboard = () => {
   return (
     <div>
       {/* Status Grid */}
       <StatusGrid />
-      {/* ChartSection */}
+      {/* Chart Section */}
       <ChartSection />
+      <div className="grid grid-cols-1 xl:grid-cols-3 gap-6">
+        <div className="xl:col-span-2">
+          <TableSection />
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/Dashboard/TableSection.tsx
@@ -0,0 +1,16 @@
+
+const TableSection = () => {
+  return (
+    <div className="space-y-6">
+      {/* Recent Order */}
+      <div className="bg-white/80 dark:bg-slate-900/80 backdrop-blur-xl rounded-b-xl border border-slate-200/50 dark:border-slate-700/50 overflow-hidden">
+        <div className="p-6 border-b border-slate-200/50 dark:border-slate-700/50 ">
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default TableSection
+
